0168e67810b9579c5fb680bd4f69eaadc01bd82f
pedrobido <pedrobido.ph@gmail.com>
2020-05-28 15:21:16 -0300

    Organizando código com namespaces

--- app/ts/views/NegociacoesView.ts
+++ app/ts/views/NegociacoesView.ts
@@ -1,42 +1,36 @@
-class NegociacoesView {
+namespace Views {
 
-    private _elemento: Element;
+    export class NegociacoesView extends Views.View<Negociacoes> {
 
-    constructor(seletor: string) {
-        this._elemento = document.querySelector(seletor);
-    }
+        template(model: Negociacoes): string {
+            return `
+                <table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>DATA</th>
+                            <th>QUANTIDADE</th>
+                            <th>VALOR</th>
+                            <th>VOLUME</th>
+                        </tr>
+                    </thead>
+        
+                    <tbody>
+                    ${model.retornaArray().map(negociacao =>
+                `<tr>
+                            <td>${negociacao.data.getUTCDate()}/${negociacao.data.getUTCMonth() + 1}/${negociacao.data.getUTCFullYear()}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                            <td>${negociacao.volume}</td>
+                        </tr>                        
+                        `).join('')} 
+                    </tbody>
+        
+                    <tfoot>
+                    </tfoot>
+                </table>               
+                `
+        }
 
-    update(model: Negociacoes): void {
-        this._elemento.innerHTML = this.template(model);
-    }
-
-    template(model: Negociacoes): string {
-        return `
-            <table class="table table-hover table-bordered">
-                <thead>
-                    <tr>
-                        <th>DATA</th>
-                        <th>QUANTIDADE</th>
-                        <th>VALOR</th>
-                        <th>VOLUME</th>
-                    </tr>
-                </thead>
-    
-                <tbody>
-                ${model.retornaArray().map(negociacao =>
-            `<tr>
-                        <td>${negociacao.data.getUTCDate()}/${negociacao.data.getUTCMonth() + 1}/${negociacao.data.getUTCFullYear()}</td>
-                        <td>${negociacao.quantidade}</td>
-                        <td>${negociacao.valor}</td>
-                        <td>${negociacao.volume}</td>
-                    </tr>                        
-                    `).join('')} 
-                </tbody>
-    
-                <tfoot>
-                </tfoot>
-            </table>               
-            `
     }
 
 }
